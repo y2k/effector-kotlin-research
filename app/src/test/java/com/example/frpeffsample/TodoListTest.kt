ed56ad8dbb7b4c37a51b4376d0f8a9a54e31f91b
y2k <itwisterlx@gmail.com>
2023-06-04 13:30:10 +0200

    add test

---
+++ app/src/test/java/com/example/frpeffsample/TodoListTest.kt
@@ -0,0 +1,46 @@
+package com.example.frpeffsample
+
+import com.example.frpeffsample.effector.Scope
+import com.example.frpeffsample.effector.allSettled
+import com.example.frpeffsample.effector.fork
+import com.example.frpeffsample.todo.addClicked
+import com.example.frpeffsample.todo.itemDeleted
+import com.example.frpeffsample.todo.storeItems
+import com.example.frpeffsample.todo.storeText
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class TodoListTest {
+
+    @Test
+    fun test_addClicked() {
+        val scope = fork(values = listOf(Scope.store(storeText, "item1")))
+
+        allSettled(addClicked, Unit, scope)
+
+        Assert.assertEquals(listOf("item1"), scope.getState(storeItems))
+    }
+
+    @Test
+    fun test_itemDeleted() {
+        val scope = fork(values = listOf(Scope.store(storeItems, listOf("item1"))))
+
+        allSettled(itemDeleted, 0, scope)
+
+        Assert.assertEquals(emptyList<String>(), scope.getState(storeItems))
+    }
+
+    @Before
+    fun before() = Dispatchers.setMain(UnconfinedTestDispatcher())
+
+    @After
+    fun after() = Dispatchers.resetMain()
+}
